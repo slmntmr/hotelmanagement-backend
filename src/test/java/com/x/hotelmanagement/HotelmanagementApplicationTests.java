a6f1654cabc5ce6bcf0df60a821efd8ca0fc740b
Süleyman TİMUR <slmn1905tmr@gmail.com>
2025-03-17 04:33:52 +0300

    v1

---
+++ src/test/java/com/x/hotelmanagement/HotelmanagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.x.hotelmanagement;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HotelmanagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
